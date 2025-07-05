a7bdc320c4d7c337a25e4e0f369a7574dc2eae93
Tanisha <tthompsonjones8771@gmail.com>
2025-07-05 14:19:41 -0400

    Change function name

--- 0002.Recursion/006.Fibonacci.py
+++ 0002.Recursion/006.Fibonacci.py
@@ -11,12 +11,12 @@ print(fib(7))
 
 # Tribonacci Series
 # 0 1 1 2 4 7 13
-def fib(n):
+def trib(n):
     if n == 0:
         return 0
     if n == 1 or n == 2:
         return 1
     
-    return fib(n-1) + fib(n-2) + fib(n-3)
+    return trib(n-1) + trib(n-2) + trib(n-3)
 
-print(fib(7))
+print(trib(7))
