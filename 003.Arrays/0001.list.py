dd04b8f007f235ec398c4b1f7a3f33a5b5d9b106
Tanisha <tthompsonjones8771@gmail.com>
2025-07-10 10:08:17 -0400

    Solve TwoSum LeetCode

---
+++ 003.Arrays/0001.list.py
@@ -0,0 +1,23 @@
+# List -- Dynamic Data Structure
+
+myList = [1,2,3,4,5,6, "Hello", True]
+# How do we access a particlar
+
+# print(myList[0])
+# print(myList[-8])
+
+# How do we add a new element to the list
+# myList.append(23)
+# At the last position
+# myList.insert(2,256)
+
+# myList[0] = 100
+
+# pop(2)
+# myList.pop()
+
+# myList.remove("Hello")
+
+# del myList
+# del myList[0]
+# print(myList)
