b4e39c74ad354aa39b9effc1611bb72ed63cb608
ArvinderPal10 <arvinderpal10@gmail.com>
2025-07-11 19:32:32 +0530

    feat: Contains Duplicate

---
+++ Leetcode/00002.Contains.py
@@ -0,0 +1,17 @@
+class Solution:
+    # def containsDuplicate(self, nums: List[int]) -> bool:
+    #     for i in range(len(nums)):
+    #         for j in range(i+1,len(nums)):
+    #             if nums[i]== nums[j]:
+    #                 return True
+        
+    #     return False
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # seen = set()
+
+        # for num in nums:
+        #     if num in seen:
+        #         return True
+        #     seen.add(num)
+        # return False
+        return len(nums) != len(set(nums))
