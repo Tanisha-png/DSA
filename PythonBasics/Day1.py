1c86e7d74c91a803373207c4e0d41c70a035895a
ArvinderPal10 <arvinderpal10@gmail.com>
2025-07-22 19:23:52 +0530

    Day 1: Python Basics

---
+++ PythonBasics/Day1.py
@@ -0,0 +1,117 @@
+# To get the output 
+# print("Hello")
+# print(1234)
+
+# print(1234,"hello")
+
+# print('Hello')
+
+# getting the output of multiple print in one line 
+# print("Hello", end=" ")
+# print("World")
+
+# Ram's Book
+# print("Ram's book")
+
+# to give input to the code 
+# print(123)
+
+# x = int(input("Enter a number: "))
+# print(x + 100)
+
+# fullname = input("Enter your name: ")
+# print(fullname)
+
+# Data Types -- Numeric -- 123 , 23.45 ,
+# str -- "", ''
+# Bool -- True , False
+
+# int -- str -- bool 
+
+# 
+# x = "1234"
+# y = int(x)
+
+# x = 1234
+# y = str(x)
+
+
+# Variables -- 
+# y = 1234
+
+# y = "2345"
+# print(type(y))
+
+# u = True
+# print(type(u))
+
+# print(type(123))
+# print(type("123"))
+# print(type(False))
+# print(type(123.345))
+
+# Write a program to find whether a number is smaller than 10 , equal to or greater than
+
+# number = int(input("Enter your number :"))
+
+# if number > 10:
+#     print("Greater than 10 ")
+# elif number < 10:
+#     print("Less than 10")
+# else:
+#     print("Equal to 10")
+
+
+# Operators -- 
+
+# Arithmetic Operators 
+# + , -, // (Interger Division) , / (Division Operator) , * , % (Modulus Operator)
+# print(23-13)
+# print(123+124)
+# print(23*5)
+
+# Answer will be in float
+# print(45/9)
+# print(45/8)
+
+# Integer Division
+# print(45//9)
+# print(45//8)
+
+
+# % -- Modulus Operator -- Remainder Operator 
+# it only returns the remainder 
+# print(10%2)
+# print(10%3)
+
+
+'''
+print("-----------: Calculator Code : --------------- ")
+
+print("""
+Select one option:
+    1. TO add Numbers
+    2. Subtract Numbers 
+    3. To multiply Numbers 
+    4. Divide Numbers 
+    5. TO find remiander""")
+
+choice = input("Enter your choice: ")
+first = int(input("Enter your first number: "))
+second = int(input("Enter the second number: "))
+
+if choice =="1":
+    print(first+second)
+elif choice =="2":
+    print(first-second)
+elif choice=="3":
+    print(first*second)
+elif choice =="4":
+    print(first/second)
+elif choice =="5":
+    print(first%second)
+else:
+    print("Invalid Choice")
+'''
+
+
---
+++ PythonBasics/Day1Questions.py
@@ -0,0 +1,12 @@
+# Write a program to find the Area of rectangle by getting the length and breath from user
+
+# Permiter of rectangle 
+
+# Square Area , Perimeter 
+# Circle , Circumference 
+
+# WAP to calculate the simple interest for a given amount , time and rate of interst
+
+# Convert the Fahrenhiet into Degree scale  (Kelvin)
+
+# Print the full name of a person by taking firstname and lastname as input
