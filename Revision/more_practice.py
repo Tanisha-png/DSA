caa132523b1e8933e70f32882cbc1904289a9183
Tanisha <tthompsonjones8771@gmail.com>
2025-09-30 09:25:03 -0400

    Add new folder with code

---
+++ Revision/more_practice.py
@@ -0,0 +1,17 @@
+# How do you write a function to print "Hello, World!"
+
+'''
+def greeting():
+    print("Hello, World")
+
+greeting()
+'''
+
+# Write a function that prints numbers from 1 to 5
+
+'''
+i = 1;
+while i < 6:
+    print(i)
+    i += 1
+'''
