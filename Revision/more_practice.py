9d3fcfeec2248dbfa2a04dc90505aeef93450d4a
Tanisha <tthompsonjones8771@gmail.com>
2025-09-30 09:46:44 -0400

    function practice

--- Revision/more_practice.py
+++ Revision/more_practice.py
@@ -15,3 +15,28 @@ while i < 6:
     print(i)
     i += 1
 '''
+
+# Write a function that shows a countdown from 10 to 1
+
+'''
+i = 10;
+while i > 0:
+    print(i)
+    i -= 1
+'''
+
+# Write a function to print even numbers from 2 to 10
+
+'''
+def even_nums(li):
+    even_nums = 2;
+
+    for i in li:
+        if i % 2 == 0:
+            even_nums += 1
+
+    print(even_nums(li))
+'''
+
+
+
