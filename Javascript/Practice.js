9e64a4e0f2d958b2f5b297616cad7e7ce5ab58a6
Tanisha <tthompsonjones8771@gmail.com>
2025-11-20 09:59:35 -0500

    Add new file

---
+++ Javascript/Practice.js
@@ -0,0 +1,25 @@
+// Factorial recursion
+
+// function factorial(n) {
+//     if (n === 1) {
+//         return 1
+//     }
+//     return n * factorial(n - 1);
+// }
+
+// console.log(factorial(5));
+
+// Print from 1 - 10
+
+// function count(n) {
+//     if (n >= 10) {
+//         return n
+//     }
+//     console.log(n)
+//     return count(n + 1)
+// }
+
+// console.log(count(1))
+
+
+
