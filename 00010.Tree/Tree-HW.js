2679d36389e7340ffac6ef33a0fb56c33e231948
Tanisha <tthompsonjones8771@gmail.com>
2026-07-23 22:31:03 -0400

    complete: find sum of all nodes

---
+++ 00010.Tree/Tree-HW.js
@@ -0,0 +1,28 @@
+// 7.A: Find sum of all nodes
+
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function countNodes(root) {
+    if (!root) {
+        return 0;
+    }
+    return 1 + countNodes(root.left) + countNodes(root.right);
+}
+
+const root = new Node(20);
+root.left = new Node(18)
+root.right = new Node(22)
+
+root.left.right = new Node(5)
+root.left.left = new Node(12)
+
+root.right.left = new Node(25)
+root.right.right = new Node(30)
+
+console.log(countNodes(root));
